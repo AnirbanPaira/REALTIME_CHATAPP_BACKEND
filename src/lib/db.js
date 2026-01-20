547c250363b6d24eb68ad5ee254b5a8dd6ef17a1
Anirban Paira <pairaanirban2002@gmail.com>
2026-01-20 17:28:10 +0530

    fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODBURI);
+        const conn = await mongoose.connect(process.env.MONGODBURI, {
+            serverSelectionTimeoutMS: 30000, // 30 seconds to select server
+            connectTimeoutMS: 30000,        // 30 seconds to connect
+            socketTimeoutMS: 45000,         // 45 seconds for socket timeout
+        });
         console.log(`Mongodb connected: ${conn.connection.host}`);
     } catch (error) {
         console.log('Mongodb connection error', error);
